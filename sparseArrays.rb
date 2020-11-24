45e46d9b4a923fd1aa650c308d386a1668e5d5f2
kalavhan <josue.brigido1@gmail.com>
2020-11-24 14:40:42 -0700

    Complete sparse array challenge

---
+++ sparseArrays.rb
@@ -0,0 +1,17 @@
+def matchingStrings(strings, queries)
+  timesFound = Array.new(queries.length) {0}
+  queries.each_with_index do |q, index|
+      strings.each_index do |i|
+          if strings[i] == q
+              timesFound[index] = timesFound[index] + 1
+          end
+      end
+  end
+  timesFound
+end
+
+s = ["lekgdisnsbfdzpqlkg","eagemhdygyv","jwvwwnrhuai","urcadmrwlqe","mpgqsvxrijpombyv","mpgqsvxrijpombyv","urcadmrwlqe","mpgqsvxrijpombyv","eagemhdygyv","eagemhdygyv","jwvwwnrhuai","urcadmrwlqe","jwvwwnrhuai","kvugevicpsdf","kvugevicpsdf","mpgqsvxrijpombyv","urcadmrwlqe","mpgqsvxrijpombyv","exdafbnobg","qhootohpnfvbl","suffrbmqgnln","exdafbnobg","exdafbnobg","eagemhdygyv","mpgqsvxrijpombyv","urcadmrwlqe","jwvwwnrhuai","lekgdisnsbfdzpqlkg","mpgqsvxrijpombyv","lekgdisnsbfdzpqlkg","jwvwwnrhuai","exdafbnobg","mpgqsvxrijpombyv","kvugevicpsdf","qhootohpnfvbl","urcadmrwlqe","kvugevicpsdf","mpgqsvxrijpombyv","lekgdisnsbfdzpqlkg","mpgqsvxrijpombyv","kvugevicpsdf","qhootohpnfvbl","lxyqetmgdbmh","urcadmrwlqe","urcadmrwlqe","kvugevicpsdf","lxyqetmgdbmh","urcadmrwlqe","lxyqetmgdbmh","jwvwwnrhuai","qhootohpnfvbl","qhootohpnfvbl","jwvwwnrhuai","lekgdisnsbfdzpqlkg","kvugevicpsdf","mpgqsvxrijpombyv","exdafbnobg","kvugevicpsdf","lekgdisnsbfdzpqlkg","qhootohpnfvbl","exdafbnobg","qhootohpnfvbl","exdafbnobg","mpgqsvxrijpombyv","suffrbmqgnln","mpgqsvxrijpombyv","qhootohpnfvbl","jwvwwnrhuai","mpgqsvxrijpombyv","qhootohpnfvbl","lekgdisnsbfdzpqlkg","eagemhdygyv","jwvwwnrhuai","kvugevicpsdf","eagemhdygyv","eagemhdygyv","lxyqetmgdbmh","qhootohpnfvbl","lxyqetmgdbmh","exdafbnobg","qhootohpnfvbl","qhootohpnfvbl","exdafbnobg","suffrbmqgnln","mpgqsvxrijpombyv","urcadmrwlqe","eagemhdygyv","lxyqetmgdbmh","urcadmrwlqe","suffrbmqgnln","qhootohpnfvbl","kvugevicpsdf","lekgdisnsbfdzpqlkg","lxyqetmgdbmh","mpgqsvxrijpombyv","jwvwwnrhuai","lxyqetmgdbmh","lxyqetmgdbmh","lekgdisnsbfdzpqlkg","qhootohpnfvbl"]
+q = ["exdafbnobg","eagemhdygyv","mpgqsvxrijpombyv","kvugevicpsdf","lekgdisnsbfdzpqlkg","kvugevicpsdf","exdafbnobg","qhootohpnfvbl","eagemhdygyv","kvugevicpsdf","suffrbmqgnln","jwvwwnrhuai","lekgdisnsbfdzpqlkg","lekgdisnsbfdzpqlkg","mpgqsvxrijpombyv","jwvwwnrhuai","kvugevicpsdf","lekgdisnsbfdzpqlkg","exdafbnobg","suffrbmqgnln","qhootohpnfvbl","eagemhdygyv","exdafbnobg","suffrbmqgnln","jwvwwnrhuai","qhootohpnfvbl","eagemhdygyv","exdafbnobg","exdafbnobg","jwvwwnrhuai","qhootohpnfvbl","lxyqetmgdbmh","qhootohpnfvbl","suffrbmqgnln","lxyqetmgdbmh","qhootohpnfvbl","eagemhdygyv","jwvwwnrhuai","eagemhdygyv","qhootohpnfvbl","mpgqsvxrijpombyv","qhootohpnfvbl","jwvwwnrhuai","exdafbnobg","eagemhdygyv","eagemhdygyv","kvugevicpsdf","kvugevicpsdf","jwvwwnrhuai","urcadmrwlqe","lxyqetmgdbmh","qhootohpnfvbl","exdafbnobg","exdafbnobg","eagemhdygyv","qhootohpnfvbl","exdafbnobg","exdafbnobg","lekgdisnsbfdzpqlkg","jwvwwnrhuai","eagemhdygyv","urcadmrwlqe","kvugevicpsdf","lekgdisnsbfdzpqlkg","jwvwwnrhuai","eagemhdygyv","lekgdisnsbfdzpqlkg","exdafbnobg","kvugevicpsdf","jwvwwnrhuai","exdafbnobg","lxyqetmgdbmh","exdafbnobg","lxyqetmgdbmh","jwvwwnrhuai","mpgqsvxrijpombyv","eagemhdygyv","urcadmrwlqe","kvugevicpsdf","qhootohpnfvbl","jwvwwnrhuai","eagemhdygyv","urcadmrwlqe","urcadmrwlqe","exdafbnobg","qhootohpnfvbl","exdafbnobg","eagemhdygyv","exdafbnobg","jwvwwnrhuai","eagemhdygyv","jwvwwnrhuai","mpgqsvxrijpombyv","urcadmrwlqe","urcadmrwlqe","eagemhdygyv","eagemhdygyv","jwvwwnrhuai","suffrbmqgnln","eagemhdygyv"]
+
+res = matchingStrings s, q
+p res
